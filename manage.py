75629e98c24c1374e36d02b853d9f734f581d80e
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2021-08-10 16:17:12 +0530

    Settings

--- manage.py
+++ manage.py
@@ -5,12 +5,6 @@ import sys
 
 
 def main():
-    if os.getcwd() != '/app':
-        try:
-            os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notes.local_settings')
-        except:
-            print('Looks Like the local settings are missing. Importing from production settings')
-            os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notes.settings')
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notes.settings')
     try:
         from django.core.management import execute_from_command_line
