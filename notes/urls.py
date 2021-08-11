da29947f61a1f9e1d3de658362fda5d21660d54d
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2021-08-11 12:30:05 +0530

    Re-Deploy

--- notes/urls.py
+++ notes/urls.py
@@ -18,7 +18,6 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 from allauth.account import views as auth_views
-from filebrowser.sites import site
 from lazynotes import views
 urlpatterns = [
     path('admin/', admin.site.urls),
